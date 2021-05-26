c3386cf7897ea93cc79ab776b1fe89ae8fa9bc4d
unknown <antran11@uh.edu>
2021-05-25 19:43:28 -0500

    Addded multiple users allowed on server and a temp gui for the user

--- chatClient.py
+++ chatClient.py
@@ -1,6 +1,8 @@
 import socket
 import threading
 
+from tkinter import *
+
 class client():
     def __init__(self):
         self.PORT = 5000
@@ -8,35 +10,61 @@ class client():
         self.ADDRESS = (self.HOST, self.PORT)
         self.FORMAT = "utf-8"
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.startCli()
+
+        self.Window = Tk()
+        self.Window.geometry("400x600")
+        self.labelTop = Label(self.Window, bg = 'green', height = 5)
+        self.labelTop.place(relwidth = 1)
+        self.entryName = Entry(self.labelTop, bg = 'white')
+        self.entryName.place(relwidth = 0.40, relheight = 0.3, relx = 0.10, rely = 0.03)
+        self.entryName.focus()
+        self.startButton = Button(self.labelTop, text = "Start chat", command = self.startCli)
+        self.startButton.place(relx = 0.5, rely = 0.005)
+
+        self.labelBottom = Label(self.Window, bg = 'red', height = 5)
+        self.labelBottom.place(relwidth = 1, rely = .75)
+        self.entryMessage = Entry(self.labelBottom, bg = 'white')
+        self.entryMessage.place(relwidth = 0.7, relheight = 0.3, relx = 0.1, rely = 0.03)
+        self.sendButton = Button(self.labelBottom, text = "Start chat", command = lambda : self.sending(self.entryMessage.get()))
+        self.sendButton.place(relx = 0.7, rely = 0.005)
+
+        self.Window.protocol("WM_DELETE_WINDOW", self.stop)
+        self.Window.mainloop()
+        self.Window.quit()
+
+    def sending(self, data):
+        self.entryMessage.delete(0, END)
+        snd = threading.Thread(target = self.sendData(data))
+        snd.start()
+
+    def stop(self):
+        self.running = False
+        self.Window.destroy()
+        self.sock.close()
+        exit(0)
 
     def startCli(self):
         self.sock.connect(self.ADDRESS)
+        self.name = self.entryName.get()
         rcv = threading.Thread(target = self.handleRecv)
         rcv.start()
-        snd = threading.Thread(target = self.sendData)
-        snd.start()
 
     def handleRecv(self):
         SIZE = 1024
         while True:
             try:
                 data = self.sock.recv(SIZE).decode(self.FORMAT)
-                if(data):
+                if(data == 'NAME'):
+                    self.sock.send(self.name.encode(self.FORMAT))
+                elif(data):
                     print(data)
             except:
                 print("An error occurred")
                 self.sock.close()
                 break
 
-    def sendData(self):
-        while True:
-            data = input("Enter your message: ")
-            self.sock.send(data.encode(self.FORMAT))
-            end = input('End connection(y/n)? ')
-            if(end == 'y'):
-                self.sock.close()
-                break
+    def sendData(self, data):
+        self.sock.send((f"{self.name}: " + data).encode(self.FORMAT))
 
 
 if __name__ == "__main__":
--- chatServer.py
+++ chatServer.py
@@ -8,6 +8,8 @@ class threadServer():
         self.HOST = ''
         self.ADDRESS = (self.HOST, self.PORT)
         self.FORMAT = "utf-8"
+        self.SIZE = 1024
+        self.clients, self.names = [], []
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind(self.ADDRESS)
         self.startSer()
@@ -17,22 +19,34 @@ class threadServer():
         while True:
             client, address = self.sock.accept()
             client.settimeout(60)
+            self.clients.append(client)
+            client.send('NAME'.encode(self.FORMAT))
+            name = client.recv(self.SIZE).decode(self.FORMAT)
+            self.names.append(name)
+            print(f"{name}")
+            self.sendData(f"{name} has joined the chat.".encode(self.FORMAT))
             thread = threading.Thread(target = self.handleReq, args = (client, address))
             thread.start()
 
     def handleReq(self, client, address):
-        SIZE = 1024
         print(f"Connected to : {str(address)}")
         while True:
             try:
-                data = client.recv(SIZE)
+                data = client.recv(self.SIZE)
                 if(data):
-                    self.sendData(client, data)
+                    self.sendData(data)
             except:
+                index = self.clients.index(client)
+                self.clients.remove(client)
                 client.close()
+                name = self.names[index]
+                del self.names[index]
+                self.sendData(f"{name} has left the chat.".encode(self.FORMAT))
                 break
-    def sendData(self, client, data):
-        client.send(("You sent : ".encode(self.FORMAT) + data))
+    def sendData(self, data):
+        for client in self.clients:
+            client.send(data)
+        
 
 if __name__ == "__main__":
     tS = threadServer()
